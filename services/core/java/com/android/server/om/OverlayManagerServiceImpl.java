18ad390f2d988c4bf6a34d7a38b3a5f9be8e5fa5
LuK1337 <priv.luk@gmail.com>
2020-10-29 12:46:05 +0100

    Allow lineage resources package to be overlayed by RRO packages
    Change-Id: I152ef9a87b16338e14af508e0f1f42830688c5ea

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -886,7 +886,7 @@ final class OverlayManagerServiceImpl {
         final OverlayInfo updatedOverlayInfo = mSettings.getOverlayInfo(overlay, userId);
         @IdmapManager.IdmapStatus int idmapStatus = IDMAP_NOT_EXIST;
 
-        if (shouldCreateIdmap(targetPackage, overlayPackage, info)) {
+        if (shouldCreateIdmap(targetPackage, overlayPackage, info) || "lineageos.platform".equals(info.getTargetPackageName())) {
             idmapStatus = mIdmapManager.createIdmap(targetPackage, overlayPackageState,
                     overlayPackage, updatedOverlayInfo.baseCodePath, overlay.getOverlayName(),
                     userId, updatedOverlayInfo.constraints);
