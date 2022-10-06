af9063501cd4aeabb8757694a9b8d6798e06b79d
Ido Ben-Hur <idoybh2@gmail.com>
2022-10-07 02:11:14 +0300

    Screenrecord: Add an option to use HEVC
    Chose the profiles that best fit the ones we have in H.264 by spec
    
    Change-Id: I153858bee5b3cce1e4cac4f86f44586a272de6ab

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -88,6 +88,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
     private static final String EXTRA_STOP_REASON = "extra_stopReason";
     private static final String EXTRA_LOW_QUALITY = "extra_lowQuality";
     private static final String EXTRA_LONGER_DURATION = "extra_longerDuration";
+    private final static String EXTRA_HEVC = "extra_HEVC";
 
     protected static final String ACTION_START = "com.android.systemui.screenrecord.START";
     protected static final String ACTION_SHOW_START_NOTIF =
@@ -118,6 +119,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
             new ScreenRecordingPreferenceUtil(this);
     private boolean mLowQuality;
     private boolean mLongerDuration;
+    private boolean mHEVC;
 
     @Inject
     public RecordingService(ScreenRecordUxController controller, @LongRunning Executor executor,
@@ -150,7 +152,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
     public static Intent getStartIntent(Context context, int resultCode,
             int audioSource, boolean showTaps,
             @Nullable MediaProjectionCaptureTarget captureTarget,
-            boolean lowQuality, boolean longerDuration) {
+            boolean lowQuality, boolean longerDuration, boolean hevc) {
         return new Intent(context, RecordingService.class)
                 .setAction(ACTION_START)
                 .putExtra(EXTRA_RESULT_CODE, resultCode)
@@ -158,7 +160,8 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
                 .putExtra(EXTRA_SHOW_TAPS, showTaps)
                 .putExtra(EXTRA_CAPTURE_TARGET, captureTarget)
                 .putExtra(EXTRA_LOW_QUALITY, lowQuality)
-                .putExtra(EXTRA_LONGER_DURATION, longerDuration);
+                .putExtra(EXTRA_LONGER_DURATION, longerDuration)
+                .putExtra(EXTRA_HEVC, hevc);
     }
 
     /**
@@ -181,9 +184,9 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
             boolean showTaps,
             int displayId,
             @Nullable MediaProjectionCaptureTarget captureTarget,
-            boolean lowQuality, boolean longerDuration) {
+            boolean lowQuality, boolean longerDuration, boolean hevc) {
         return getStartIntent(context, resultCode, audioSource, showTaps, captureTarget,
-                lowQuality, longerDuration)
+                lowQuality, longerDuration, hevc)
                 .putExtra(EXTRA_DISPLAY_ID, displayId);
     }
 
@@ -215,6 +218,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
                 mShowTaps = intent.getBooleanExtra(EXTRA_SHOW_TAPS, false);
                 mLowQuality = intent.getBooleanExtra(EXTRA_LOW_QUALITY, false);
                 mLongerDuration = intent.getBooleanExtra(EXTRA_LONGER_DURATION, false);
+                mHEVC = intent.getBooleanExtra(EXTRA_HEVC, true);
 
                 MediaProjectionCaptureTarget captureTarget =
                         intent.getParcelableExtra(EXTRA_CAPTURE_TARGET,
@@ -242,6 +246,7 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
                 );
                 setLowQuality(mLowQuality);
                 setLongerDuration(mLongerDuration);
+                setHEVC(mHEVC);
 
                 if (startRecording()) {
                     updateState(true);
@@ -619,6 +624,12 @@ public class RecordingService extends Service implements ScreenMediaRecorderList
     private void setLongerDuration(boolean longer) {
         if (getRecorder() != null) {
             getRecorder().setLongerDuration(longer);
+        }
+    }
+
+    private void setHEVC(boolean hevc) {
+        if (getRecorder() != null) {
+            getRecorder().setHEVC(hevc);
         }
     }
 
--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionContentManager.kt
@@ -110,6 +110,7 @@ class ScreenRecordPermissionContentManager(
     private lateinit var lowQualitySwitch: Switch
     private lateinit var longerDurationSwitch: Switch
     private lateinit var skipTimeSwitch: Switch
+    private lateinit var hevcSwitch: Switch
     private lateinit var tapsView: View
     private lateinit var options: Spinner
 
@@ -159,6 +160,7 @@ class ScreenRecordPermissionContentManager(
         lowQualitySwitch = containerView.requireViewById(R.id.screenrecord_lowquality_switch)
         longerDurationSwitch = containerView.requireViewById(R.id.screenrecord_longer_timeout_switch)
         skipTimeSwitch = containerView.requireViewById(R.id.screenrecord_skip_time_switch)
+        hevcSwitch = containerView.requireViewById(R.id.screenrecord_hevc_switch)
 
         tapsView = containerView.requireViewById(R.id.show_taps)
         updateTapsViewVisibility()
@@ -170,6 +172,7 @@ class ScreenRecordPermissionContentManager(
         lowQualitySwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
         longerDurationSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
         skipTimeSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
+        hevcSwitch.setOnTouchListener { _, event -> event.action == ACTION_MOVE }
 
         options = containerView.requireViewById(R.id.screen_recording_options)
         val a: ArrayAdapter<*> =
@@ -225,6 +228,7 @@ class ScreenRecordPermissionContentManager(
             else ScreenRecordingAudioSource.NONE
         val lowQuality = lowQualitySwitch.isChecked
         val longerDuration = longerDurationSwitch.isChecked
+        val hevc = hevcSwitch.isChecked
         val startIntent =
             PendingIntent.getForegroundService(
                 userContext,
@@ -238,6 +242,7 @@ class ScreenRecordPermissionContentManager(
                     captureTarget,
                     lowQuality,
                     longerDuration,
+                    hevc,
                 ),
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
             )
